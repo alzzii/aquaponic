7ec160b98025be0ea83dfa44c9e03ffb48662190
alzzii <zulqha.ariandi@gmail.com>
2025-08-05 21:13:32 +0700

    initial commit

--- db.js
+++ db.js
@@ -1,15 +1,24 @@
-// db.js
 const mysql = require('mysql2');
 require('dotenv').config();
+const { URL } = require('url');
+
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+  console.error('❌ DATABASE_URL tidak ditemukan di env');
+  process.exit(1);
+}
+
+const parsedUrl = new URL(dbUrl);
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'Alzikri1201',
-  database: process.env.DB_NAME || 'aquaponic_monitoring'
+  host: parsedUrl.hostname,
+  port: parsedUrl.port || 3306,
+  user: parsedUrl.username,
+  password: parsedUrl.password,
+  database: parsedUrl.pathname.replace('/', '')
 });
 
-// Coba koneksi saat file diimpor
 db.connect((err) => {
   if (err) {
     console.error('❌ Gagal koneksi ke MySQL:', err.message);
